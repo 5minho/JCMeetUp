f0942de1de92fffd9c2e1b13801f6e3a96358b28
minomi <5minhominho@gmail.com>
2018-03-16 22:36:50 +0900

    O(N) vs O(2N) performance test

--- JCAlgorithms/JCAlgorithmsTest/JCAlgorithmsTest.swift
+++ JCAlgorithms/JCAlgorithmsTest/JCAlgorithmsTest.swift
@@ -10,6 +10,13 @@ import XCTest
 
 class JCAlgorithmsTest: XCTestCase {
     
+    private var array = [Int](1...1000000)
+    
+    override func setUp() {
+        super.setUp()
+        self.shuffle(array: &array)
+    }
+    
     func testGCDLCM() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
@@ -54,6 +61,7 @@ class JCAlgorithmsTest: XCTestCase {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
+            
         }
     }
     
@@ -68,6 +76,39 @@ class JCAlgorithmsTest: XCTestCase {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
+        }
+    }
+    
+    func testNLoop() {
+        self.measure {
+            var min = Int.min
+            var max = Int.max
+            for x in array {
+                if x < min { min = x }
+                if x > max { max = x }
+            }
+        }
+    }
+    
+    func test2NLoop() {
+        self.measure {
+            var min = Int.min
+            var max = Int.max
+            for x in array {
+                if x < min { min = x }
+            }
+            for x in array {
+                if x > max { max = x }
+            }
+        }
+    }
+    
+    private func shuffle(array : inout [Int]) {
+        var last = array.count - 1
+        while last > 0 {
+            let rand = Int(arc4random_uniform(UInt32(last)))
+            array.swapAt(last, rand)
+            last -= 1
         }
     }
     
