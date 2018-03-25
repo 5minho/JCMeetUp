2bd7d1853f3addf3014210c351beb3292584b85b
minomi <5minhominho@gmail.com>
2018-03-25 22:42:38 +0900

    변수명 좀더 명확하게 변경

--- JCAlgorithms/JCAlgorithms/BinaryGap.swift
+++ JCAlgorithms/JCAlgorithms/BinaryGap.swift
@@ -9,27 +9,28 @@
 import Foundation
 
 extension Int {
-    var isOdd : Bool {
-        return (self & 1) == 0
+    // 가장 왼쪽에 있는 비트
+    var lsb : Int {
+        return (self & 1)
     }
 }
 
 public func solution(_ N : Int) -> Int {
     var num = N
-    
-    while num.isOdd {
-        num >>= 1
-    }
+    // num의 LSB(least significant bit)가 1이 될때 까지 비트를 오른쪽으로 밀어준다.
+    while num.lsb != 1 { num >>= 1 }
     
     var count = 0
     var answer = 0
     
     while num != 0 {
         num >>= 1
-        if num.isOdd {
+        // LSB가 0 이면 count를 증가시킨다
+        if num.lsb == 0 {
             count += 1
             continue
         }
+        // LSB가 1이면 이전에 저장해둔 count와 비교해서 제일 큰 BinaryGap 을 구한다.
         answer = max(answer, count)
         count = 0
     }
