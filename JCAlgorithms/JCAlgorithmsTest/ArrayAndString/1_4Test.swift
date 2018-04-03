f81401c065a69759dc183199c603f24d5941b58e
minomi <5minhominho@gmail.com>
2018-04-03 23:34:34 +0900

    TDD로 1_4 풀어보기 green - 가능한 심플하게 통과코드 작성하기

--- JCAlgorithms/JCAlgorithmsTest/ArrayAndString/1_4Test.swift
+++ JCAlgorithms/JCAlgorithmsTest/ArrayAndString/1_4Test.swift
@@ -15,13 +15,46 @@ import XCTest
  refactor : 리팩토링 (필요하다면 다시 red)
 */
 
+extension Character {
+    var asciiValue: Int {
+        let s = self.unicodeScalars
+        return Int(s[s.startIndex].value)
+    }
+}
+
+extension Int {
+    var isOdd : Bool {
+        return (self & 1) == 1
+    }
+}
+
 extension String {
     var isPermutationOfPalindrome : Bool {
-        return false
+        var alphaCountList = [Int](repeating: 0, count: 26)
+        let lowercaseStr = self.lowercased()
+        var oddCount = 0
+        lowercaseStr.forEach {
+            let asciiValue = $0.asciiValue
+            if (97..<123).contains(asciiValue) {
+                alphaCountList[$0.asciiValue - 97] += 1
+            }
+        }
+        alphaCountList.forEach {
+            if $0.isOdd {
+                oddCount += 1
+            }
+        }
+        return oddCount.isOdd
     }
 }
 
 class ArrayAndString1_4Test: XCTestCase {
+    
+    func test_Swift_Character의_아스키코드를_잘_가져오는지_확인() {
+        let char = Character("a")
+        let expected = 97
+        XCTAssertEqual(expected, char.asciiValue)
+    }
     
     func test_주어진_문자열이_회문의_순열인지_확인() {
         let string = "Tact Coa"
