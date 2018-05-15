dd1c6e35aac546768a2221d7589e6dfa86f932ba
minomi <5minhominho@gmail.com>
2018-05-15 23:15:12 +0900

    stack container 테스트 수정

--- StackAndQueue/test_stack.py
+++ StackAndQueue/test_stack.py
@@ -1,12 +1,21 @@
 import unittest
 from stack import StackContainer
 
-class TestStackContainer(unittest.TestCase):
 
-    def __init__(self):
-        self.container = None
+class TestStackContainer(unittest.TestCase):
 
     def setUp(self):
         self.container = StackContainer()
 
+    def test_push(self):
+        self.fail()
 
+    def test_pop(self):
+        self.fail()
+
+    def test_get_next_push_idx(self):
+        self.fail()
+
+
+if __name__ == '__main__':
+    unittest.main()
